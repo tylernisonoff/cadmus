5cef0c97ca78f4e76fdb646c405e384c9fa7815d
Phips Peter <pspeter333@gmail.com>
2014-06-06 12:06:53 -0700

    Starting to get the authorization code working
    Plus we have some sweet Willy Wonka and the chocolate factory quotes.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-string-literal */
 export var ASANA_STRATEGY = {
-    CLIENT_ID: process.env["ASANA_CLIENT_ID"],
-    CLIENT_SECRET: process.env["ASANA_CLIENT_SECRET"],
-    CALLBACK_URL: process.env["ASANA_CLIENT_CALLBACK"]
+    clientID: process.env["ASANA_CLIENT_ID"],
+    clientSecret: process.env["ASANA_CLIENT_SECRET"],
+    callbackURL: process.env["ASANA_CLIENT_CALLBACK"]
 };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 /// <reference path="../typings/express/express.d.ts" />
+/// <reference path="../typings/passport/passport.d.ts" />
+/// <reference path="../typings/passport-asana/passport-asana.d.ts" />
+import config = require("./config");
 import express = require("express");
+import passport = require("passport");
+import passportAsana = require("passport-asana");
 
 var app = express();
 
-app.get("/", (req, res) => {
-   res.json({
-       "ok": true
-   });
+console.log(config);
+
+passport.use(new passportAsana.Strategy(config.ASANA_STRATEGY, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken, refreshToken, profile);
+    done(null, profile);
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/auth/asana", passport.authenticate("Asana", {}));
+app.get("/auth/asana/callback", passport.authenticate("Asana", {
+    successRedirect: "/winning",
+    failureRedirect: "/losing"
+}));
+
+app.get("/winning", (req, res) => {
+    res.send("So shines a good deed in a weary world.");
+});
+
+app.get("/losing", (req, res) => {
+    res.send("You lose! Good day, sir!");
 });
 
 app.listen(3000, () => {
