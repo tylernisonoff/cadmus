544f1354ad020be0d1fb06a0340466ecbfdb22cc
Phips Peter <pspeter333@gmail.com>
2014-06-06 10:58:11 -0700

    Setting up the basic express app
    Now we can at least handle the express service simple with the
    TypeScript and run the code with npm start. This still does not solve
    how it will work on heroku though.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Cadmus");
+/// <reference path="../typings/express/express.d.ts" />
+import express = require("express");
+
+var app = express();
+
+app.get("/", (req, res) => {
+   res.send("Hello World!!!");
+});
+
+app.listen(3000, () => {
+   console.log("Welcome Cadmus!");
+});
