02948fa61df45ff34d23ccf071e7497e5913abb6
Phips Peter <pspeter333@gmail.com>
2014-06-06 11:31:38 -0700

    Starting configuration
    The secrets should be kept in a config.sh script that is never checked
    into git. This means that integration testing is probably not likely for
    authentication.

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+/* tslint:disable:no-string-literal */
+export var ASANA_STRATEGY = {
+    CLIENT_ID: process.env["ASANA_CLIENT_ID"],
+    CLIENT_SECRET: process.env["ASANA_CLIENT_SECRET"],
+    CALLBACK_URL: process.env["ASANA_CLIENT_CALLBACK"]
+};
