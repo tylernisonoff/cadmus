075993733f71ad39a8b50c24ad18f4db378e202e
Phips Peter <pspeter333@gmail.com>
2014-06-20 11:33:29 -0700

    Switching to CamelCase for columns
    CamelCase is more natural with TypeScript

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS credentials (
     id bigserial PRIMARY KEY UNIQUE NOT NULL,
     service text NOT NULL,
-    access_token text NOT NULL,
-    refersh_token text NOT NULL,
-    user_id int NOT NULL REFERENCES users (id)
+    accessToken text NOT NULL,
+    refershToken text NOT NULL,
+    userId int NOT NULL REFERENCES users (id)
 );
