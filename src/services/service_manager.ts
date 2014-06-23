fbf710824b42a6045185ce6eb939ba1c336c5deb
Phips Peter <pspeter333@gmail.com>
2014-06-22 18:28:01 -0700

    Starting to put together services

--- src/services/service_manager.ts
+++ src/services/service_manager.ts
@@ -1,7 +1,9 @@
 /* tslint:disable:no-string-literal */
+/// <reference path="../../typings/express/express.d.ts" />
 /// <reference path="../../typings/passport/passport.d.ts" />
 /// <reference path="./strategy_config.ts" />
 import Datastore = require("../datastore/datastore");
+import express = require("express");
 import passport = require("passport");
 import Service = require("./service");
 import util = require("util");
@@ -23,9 +25,46 @@ class ServiceManager {
         };
     }
 
+    private authUrl(name: string): string {
+        return util.format("/auth/%s", name.toLowerCase());
+    }
+
+    private callbackUrl(name: string): string {
+        return util.format("/auth/%s/callback", name.toLowerCase());
+    }
+
     public registerService(service: Service, authenticates: boolean = false): void {
         this.serviceMap[service.name] = authenticates;
         passport.use(new service.Strategy(this.config(service), service.callback));
+    }
+
+    public init(app: express.Application): void {
+        passport.serializeUser((user, done) => {
+            done(null, user.id);
+        });
+
+        passport.deserializeUser((id, done) => {
+            this.datastore.getUser(id).nodeify(done);
+        });
+
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        Object.keys(this.serviceMap).forEach((key) => {
+            if (this.serviceMap[key]) {
+                app.get(this.authUrl(key), passport.authenticate(key, {}));
+                app.get(this.callbackUrl(key), passport.authenticate(key, {
+                    successRedirect: "/winning",
+                    failureRedirect: "/losing"
+                }));
+            } else {
+                app.get(this.authUrl(key), passport.authorize(key, {}));
+                app.get(this.callbackUrl(key), passport.authorize(key, {
+                    successRedirect: "/winning",
+                    failureRedirect: "/losing"
+                }));
+            }
+        });
     }
 }
 
