c0e4758521d2cddc9d23055d1176d43ae6304afa
Phips Peter <pspeter333@gmail.com>
2014-07-08 11:51:43 -0700

    Adding connections to schema
    This will represent various users found in the service. There is the
    potential to move services out of the database as strings and to their
    own table but I'm not worried about that right now.

--- schema.sql
+++ schema.sql
@@ -11,3 +11,18 @@ CREATE TABLE IF NOT EXISTS credentials (
     refresh_token text NOT NULL,
     user_id bigint NOT NULL REFERENCES users (id)
 );
+
+CREATE TABLE IF NOT EXISTS connections (
+    id bigserial PRIMARY KEY UNIQUE NOT NULL,
+    service text NOT NULL,
+    uid text NOT NULL,
+    name text NOT NULL,
+    data json NOT NULL,
+    task_id bigint UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS credentials_connections (
+    credential_id bigint NOT NULL REFERENCES credentials (id),
+    connection_id bigint NOT NULL REFERENCES connections (id),
+    UNIQUE(credential_id, connection_id)
+);
